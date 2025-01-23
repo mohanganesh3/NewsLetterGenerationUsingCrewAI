2a822aa2b7d1e50851e0f58d09340f121e7788b1
Mohan Ganesh <mohanganesh@Mohans-MacBook-Air.local>
2025-01-23 21:19:47 +0530

    first commit

---
+++ src/newsletter_gen/main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+from newsletter_gen.crew import NewsletterGenCrew
+
+def load_html_template(): 
+    with open('src/newsletter_gen/config/newsletter_template.html', 'r') as file:
+        html_template = file.read()
+        
+    return html_template
+
+
+def run():
+    # Replace with your inputs, it will automatically interpolate any tasks and agents information
+    inputs = {
+        'topic': input('Enter the topic for yout newsletter: '),
+        'personal_message': input('Enter a personal message for your newsletter: '),
+        'html_template': load_html_template()
+    }
+    try:
+        NewsletterGenCrew().crew().kickoff(inputs=inputs)
+    except Exception as e:
+        print(f"An error occurred: {e}")
